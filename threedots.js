0447533043321c5299a88f662c85aeae74be012f
Uchchash346 <mru416@gmail.com>
2021-02-01 19:43:39 +0600

    first commit

---
+++ threedots.js
@@ -0,0 +1,10 @@
+const ages = [10,12,15,85,63];
+const ages2 = [51,14,52,88,45,69,36,63];
+const ages3 = [155,12,54,52,63,25,21];
+
+const allAges = [...ages, ...ages2, ...ages3];
+//console.log(allAges)
+
+const takaPoisa = [650,450,122,136,255,258];
+const maximum = Math.max(...takaPoisa);
+console.log(takaPoisa);
