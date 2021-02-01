0d9f0ca48604d7f369d49ac6086005f1162e3dbb
Uchchash346 <mru416@gmail.com>
2021-02-01 12:09:34 +0600

    first commit

---
+++ template.js
@@ -0,0 +1,14 @@
+const firstName = "Justin";
+const lastName = "Timberlake";
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName} ${20+50+30} is a good boy`;
+
+
+const multiline = "I love You\n"
+    + "I miss You\n"
+    + "I need you"
+
+const multiline2 = `I love you
+I miss you
+I am Uchchash`
+console.log(multiline2);
