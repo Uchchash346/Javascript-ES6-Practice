961233f555c629b44855b97201a5913776f03ba1
Uchchash346 <mru416@gmail.com>
2021-02-01 09:06:37 +0600

    first commit

---
+++ letconst.js
@@ -0,0 +1,13 @@
+const myName = "Rahim";
+//myName = "Rahman"
+console.log(myName);
+
+let patientName = "karim chacha";
+patientName = "Uchchash";
+console.log(patientName);
+
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i)
