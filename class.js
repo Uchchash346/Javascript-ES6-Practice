788a591981593f8b43f1718147fa818c0e8a5242
Uchchash346 <mru416@gmail.com>
2021-02-01 20:43:07 +0600

    first commit

---
+++ class.js
@@ -0,0 +1,10 @@
+class student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "Kolimunnesa School"
+    }
+}
+const student1 = new student(12, "Shuvo");
+const student2 = new student(22, "mahiya");
+console.log(student1)
