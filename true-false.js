6474aee76be1026c9d95d3108f15f926b5a2d4bb
Uchchash346 <mru416@gmail.com>
2021-02-02 10:48:19 +0600

    first commit

---
+++ true-false.js
@@ -0,0 +1,26 @@
+/*const age = 0; // If age = 0, result is false
+if (age){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}*/
+
+const name = "Uchchash"; // Without any string , it says false
+if (name.length){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
+
+//Falsy:
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+
+//Truthy
+//'0', ' ', [], {},"false"
